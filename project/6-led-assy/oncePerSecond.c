c6511d4743ff9615a9718f615b1260ce04f3f80f
CRobin122 <136745606+CRobin122@users.noreply.github.com>
2025-08-04 08:01:33 -0600

    Create oncePerSecond.c

---
+++ project/6-led-assy/oncePerSecond.c
@@ -0,0 +1,6 @@
+void oncePerSecond() // repeatedly start bright and gradually lower duty cycle, one step/sec
+{
+  blinkLimit ++;  // reduce duty cycle
+  if (blinkLimit >= 8)  // but don't let duty cycle go below 1/7.
+    blinkLimit = 0;
+}
